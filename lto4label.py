f66eefeaca5e98e59b637d46ffaa0ddda3827d40
Ian Mackinnon <imackinnon@gmail.com>
2011-08-26 12:21:15 +0100

    Removed Mako dependencies; now fully standard libaray. Created tests. Created Makefile.

--- lto4label.py
+++ lto4label.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import sys
+
 from code39 import encode_39
 
-from mako.template import Template
+from StringIO import StringIO
 
 from optparse import OptionParser
+
+import logging
+
+
+
+log = logging.getLogger('lto4label')
+
+
 
 class Label(object):
     
@@ -27,7 +37,11 @@ class Label(object):
 
     def __init__(self, number):
         number = int(number)
-        assert 0 <= number <= 999999
+        try:
+            assert 0 <= number <= 999999
+        except AssertionError, e:
+            log.error("%d: Number is out of range. Must be between 0 and 999999 inclusive." % number)
+            sys.exit()
         self.number = number
     
     @property
@@ -50,11 +64,93 @@ class Label(object):
             c.append((n, self._colors[n]))
         return c
 
+
+
+def mm_to_px(mm):
+    return mm * 90 / 25.4
+
+
+
+def render(width, height, margin, rows, columns, labels, legend=""):
+    r = StringIO()
+    r.write("""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   width="{width}mm"
+   height="{height}mm"
+   version="1.1"
+   style="font-family:freesans, sans">
+""".format(width=width, height=height))
+
+    x = margin
+    y = margin
+    while labels:
+        label = labels.pop()
+        assert x + 78 < (width - margin), "Too many for page"
+        if  y + 17 > (height - margin):
+            x += 78 + margin
+            y = margin
+        r.write("""  <g transform="translate({xpx},{ypx})">
+    <rect ry="1mm" rx="1mm" y="0" x="0" height="17mm" width="78mm" style="fill:none;stroke:black;stroke-opacity:.2;stroke-width:0.1mm;" />
+    <g transform="scale(1.55,1)">
+""".format(xpx=mm_to_px(x), ypx=mm_to_px(y)))
+
+        cx = 10
+        for f in label.code_39:
+            if f in 'bB':
+                r.write("""      <rect y="0" x="{x}" height="17mm" width="{width}" style="fill:black;stroke-width:0;" />
+""".format(x=cx, width=1 + 2 * int(f == "B")))
+            cx += 1 + 2 * int(f in "BW") 
+        r.write("""    </g>
+""")
+
+        nwidth = 10.3
+        nx = 3
+
+        for n, color in label.color_numbers:
+            r.write("""    <rect y="0" x="{rectx}mm" height="5mm" width="{width}mm" style="fill:{color};stroke:#000000;stroke-width:0.1mm;" />
+    <text y="3.9mm" x="{textx}mm" style="font-size:4mm; text-anchor: middle;">{n}</text>
+""".format(rectx=nx, width=nwidth, color=color, textx=nx + nwidth / 2, n=n))
+
+            nx += nwidth
+        r.write("""    <rect y="0" x="{rectx}mm" height="5mm" width="{width}mm" style="fill:white;stroke:#000000;stroke-width:0.1mm;" />
+    <text y="3.3mm" x="{textx}mm" style="font-size:2.5mm; text-anchor: middle;">L4</text>
+  </g>
+""".format(rectx=nx, width=nwidth, textx=nx + nwidth / 2))
+
+        y += 17 + margin
+
+    r.write("""</svg>
+""")
+    
+    return r.getvalue()
+    
+    
+
+
+
 if __name__ == "__main__":
-    usage = "Usage: %prog TAPENUMBER..."
+    log.addHandler(logging.StreamHandler())
+    log.setLevel(logging.WARNING)
+
+    usage = """Usage: %prog TAPENUMBER...
+
+TAPENUMBER... :  Up to 20 integers between 0 and 999999 inclusive."""
+
     parser = OptionParser(usage=usage)
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbosity",
+                      help="Print verbose information for debugging.", default=None)
+    parser.add_option("-q", "--quiet", action="store_false", dest="verbosity",
+                      help="Suppress warnings.", default=None)
 
     (options, args) = parser.parse_args()
+
+    if options.verbosity:
+        log.setLevel(logging.INFO)
+    elif options.verbosity is False:
+        log.setLevel(logging.ERROR)
+
     if not args:
         parser.print_usage()
         quit()
@@ -78,10 +174,8 @@ if __name__ == "__main__":
     for arg in args:
         labels.append(Label(arg))
 
-    
-    label_template = Template(filename="template/label.svg")
     if len(labels) > rows * columns:
-        print "Too many for page"
-        quit()
+        log.error("Error. Too many labels for page. Maximum is 20.")
+        sys.exit()
 
-    print label_template.render(width=width, height=height, margin=margin, rows=rows, columns=columns, labels=labels, legend="Disable 'Scale to fit page' in print options (print at '100%' size).")
+    print render(width=width, height=height, margin=margin, rows=rows, columns=columns, labels=labels, legend="Disable 'Scale to fit page' in print options (print at '100%' size).")
